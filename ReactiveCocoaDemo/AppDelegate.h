a3fd1b865e7c053b42c19ddeba8c6157f9169c1f
张新 <zhangxin@zhangxindemacbook-pro.local>
2016-05-12 22:32:31 +0800

    Initial Commit

---
+++ ReactiveCocoaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReactiveCocoaDemo
+//
+//  Created by 张新 on 16/5/12.
+//  Copyright © 2016年 zhangxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
